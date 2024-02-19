265af35fb0ad5da35ac00d6e193290357a4e0ab8
carlos soto <8994@holbertonstudents.com>
2024-02-19 15:02:26 -0400

    Exerc.6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -15,9 +15,9 @@ int main(void)
 	long long int; lld
 
 
-	printf("Size of a char: %lu.\n", (unsigned long)sizeof(c));	
+	printf("Size of a char: %lu.\n", (unsigned long)sizeof(c));
 	printf("Size of an int: %lu.\n", (unsigned long)sizeof(i));
-	printf("Size of a long int: %lu.\n", (unsigned long)sizeof(ld));	
+	printf("Size of a long int: %lu.\n", (unsigned long)sizeof(ld));
 	printf("Size of a long long int: %lu.\n", (unsigned long)sizeof(lld));
 	printf("Size of of a float: %lu.\n", (unsigned long)sizeof(f));
 
