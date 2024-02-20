85d5199c3dd4ac73292873163d95de8c8681c632
carlos soto <8994@holbertonstudents.com>
2024-02-19 23:14:27 -0400

    9

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <studio.h>
-
+#include <unistd.h>
 /**
  * main - Entry point
- * Return: Always 0 (Success)
+ * Return 1
  */
 int main(void)
 {
 	printf("and that piece of art is useful\e" - Dora Korpar, 2015-10-19\n");
-	return (0);
+	return (1);
 }
