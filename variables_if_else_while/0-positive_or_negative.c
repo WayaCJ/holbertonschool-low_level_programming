f7447901e2234b57e7f110cb9b95686633eb43ac
carlos soto <8994@holbertonstudents.com>
2024-02-20 10:29:45 -0400

    If statements

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
@@ -10,14 +11,17 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 
-	if ( n>0 ) {
-	printf("is positive\n");
+	if (n > 0)
+	{
+	printf("%d is positive\n");
 	}
-	else if ( n==0 ) {
-	printf("is zero\n");
+	else if (n == 0)
+	{
+	printf("%d is zero\n");
 	}
-	else ( n<0 ) {
-	printf("is negative\n");
+	else if (n < 0)
+	{
+	printf("%d is negative\n");
 	}
 	/* your code goes there */
 	return (0);
