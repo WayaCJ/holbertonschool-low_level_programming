e94d023f00d3d7a2cff87335b8c86d34c6cc45b2
carlos soto <8994@holbertonstudents.com>
2024-02-20 10:11:39 -0400

    If statements

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -5,17 +5,18 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-	int n;
+int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
 	if ( n>0 ) {
 	printf("is positive\n");
 	}
-	if ( n=0 ) {
+	else if ( n==0 ) {
 	printf("is zero\n");
 	}
-	if ( n<0 ) {
+	else ( n<0 ) {
 	printf("is negative\n");
 	}
 	/* your code goes there */
