a8e234b466267d393bb1914c5a6a7c782c55a663
carlos soto <8994@holbertonstudents.com>
2024-02-21 17:36:35 -0400

    6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_abs- print the sign of a number
+ *@n: number being computed
+ * Return: Absolute value
+ */
+int _abs(int n)
+{
+	if (c < 0)
+	{
+	int abs_value;
+
+	abs_value = n * -1;
+	return (abs_value);
+	}
+	return (n);
+}
