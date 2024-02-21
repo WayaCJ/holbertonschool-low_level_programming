32c0d730ea527915ba66d792103defee2950f321
carlos soto <8994@holbertonstudents.com>
2024-02-21 10:29:18 -0400

    0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#include <main.h>
+#include <_putchar>
 /**
  *main- Entry point
  *
@@ -6,6 +7,6 @@
  */
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_putchar\n');
 	return (0);
 }
