60aad62c3021e07f90a3dc6c3adb47250b40969a
carlos soto <8994@holbertonstudents.com>
2024-02-22 09:57:17 -0400

    1

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_isdigit- checks for a digit
+ *@c: character
+ *Return: 1 if is a digit or 0 if otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
