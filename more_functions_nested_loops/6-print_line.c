9bd699c9722a4580beaecf2bb2dbd14479072777
carlos soto <8994@holbertonstudents.com>
2024-02-22 13:09:36 -0400

    6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *print_line- it prints a line
+ *@n: number of times
+ *
+ */
+void print_line(int n)
+{
+	int t;
+
+	for (t = 0; t < n; t++)
+	{
+		if (n > 0)
+		{
+			_putchar('_');
+		}
+	}
+		_putchar('\n');
+
+}
