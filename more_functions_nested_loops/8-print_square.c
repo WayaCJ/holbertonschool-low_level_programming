0a138f0dca0d92089bdb743a805e2880f747dd03
carlos soto <8994@holbertonstudents.com>
2024-02-22 14:22:58 -0400

    8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *print_square- prins a square
+ *
+ *@size: size of the square
+ */
+void print_square(int size)
+{
+	int t, o;
+
+	if (size > 0)
+	{
+		for (t = 0; t < size; t++)
+		{	
+			for (o = 0; o < size; o++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
