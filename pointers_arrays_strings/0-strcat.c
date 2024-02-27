bb84a773f3741bc018e8a0cf1bc42e1409038e6e
carlos soto <8994@holbertonstudents.com>
2024-02-27 10:06:10 -0400

    0

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *_strcat- cat two strings
+ *@dest: destination
+ *@src: src
+ */
+char *_strcat(char *dest, char *src)
+{
+	int a, b = 0;
+
+	while (dest[b])
+	{
+		b++;
+	}
+	for (a = 0; src[a] != '\0'; a++)
+	{
+		dest[b] = src[a];
+		b++;
+	}
+	dest[b] = '\0';
+	return (dest);
+}
