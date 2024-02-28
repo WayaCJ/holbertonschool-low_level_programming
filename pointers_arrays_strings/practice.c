5b2e0bf5bcc5dc2025aad8dfba2a6c7666d0ba31
carlos soto <8994@holbertonstudents.com>
2024-02-28 12:54:34 -0400

    practice

--- pointers_arrays_strings/practice.c
+++ pointers_arrays_strings/practice.c
@@ -6,12 +6,15 @@
  */
 int main(void)
 {
-	int number = 14, v;
+	int number = 13, v;
+	int *n;
+
+	n = &number;
 
 	while (v != number)
 	{
-		printf("Try to guess the number from 1 to 20\n");
-		printf("What number do you think is the secret number?  " );
+		printf("Try to guess the number from 1 to 50\n");
+		printf("What number do you think is the secret number?  ");
 		scanf("%d", &v);
 
 		if (number > v)
@@ -21,7 +24,7 @@ int main(void)
 		else if (number == v)
 		{
 			printf("\n");
-			printf("We can't believe it, you got it right. Congrats!\n");
+			printf("We can't believe it, you got it right.\n The secret number was %d. Congrats!\n", *n);
 		}
 		else
 		{
