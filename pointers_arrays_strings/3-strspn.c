056dcaeaeee25882275f377799d2825f53ca0391
carlos soto <8994@holbertonstudents.com>
2024-03-01 09:23:03 -0400

    3

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -8,18 +8,18 @@
 unsigned int _strspn(char *s, char *accept);
 {
 	unsigned int n = 0;
-	int b;
+	int r;
 
 	while (*s)
 	{
-		for (b = 0; accept[b]; b++)
+		for (r = 0; accept[r]; r++)
 		{
-		if (*s == accept[b])
+		if (*s == accept[r])
 		{
 			n++;
 				break;
 		}
-		else if (accept[b + 1] == '\0')
+		else if (accept[r + 1] == '\0')
 			return (n);
 		}
 		s++;
