95c3b3aacada10619cf4899420dbdc58f3d30bff
Carlos <sotocarlos1956@gmail.com>
2024-03-13 23:43:44 -0400

    1

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointer.h"
+
+/**
+ *array_iterator- funct
+ *@array: array
+ *@size: the size
+ *@action: pointer
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int a = 0;
+
+	if (array != NULL && action != && size > 0)
+	{
+		while (a < size)
+		{
+			action(array[a]);
+			a++;
+		}
+	}
+}
