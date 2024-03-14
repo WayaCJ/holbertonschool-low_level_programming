0966c5314668fe4f4a4647d1f6edf6880d62ab49
Carlos <sotocarlos1956@gmail.com>
2024-03-13 23:45:48 -0400

    1

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -5,12 +5,13 @@
  *@array: array
  *@size: the size
  *@action: pointer
+ *Return: void
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int a = 0;
 
-	if (array != NULL && action != && size > 0)
+	if (array != NULL && action != NULL && size > 0)
 	{
 		while (a < size)
 		{
